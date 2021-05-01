c1fa55af39be6352a0680ef10d8105039df8a514
carlosandressanchezaviles <carlos.sanchez7@udea.edu.co>
2021-04-30 22:15:31 -0500

    no-sql-lab

--- routes/authors.js
+++ routes/authors.js
@@ -10,12 +10,46 @@ router.get('/', async (req, res) => {
   try {
     let filters = {};
     if (req.query.pais) filters = { pais: req.query.pais };
-    const authors = await Author.find(filters);
+    const authors = await Author.find(filters,);
     res.json(authors);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
+
+router.get('/1', async (req, res) => {
+  try {
+    let filters = {};
+    filters = {publicados:{$gt:20}, pais:"Colombia"};
+    const authors = await Author.find(filters,{_id:0,publicados:0,pais:0,__v:0});
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get('/2', async (req, res) => {
+  try {
+    let filters = {};
+    filters = {apellido:{$exists:true}};
+    const authors = await Author.find(filters,{_id:0,apellido:0,publicados:0,pais:0,__v:0});
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get('/3', async (req, res) => {
+  try {
+    let filters = {};
+    filters = { $or:[{publicados:{$gt:20}}, {pais:"Argentina"}]};
+    const authors = await Author.find(filters,{_id:0,nombre:0,publicados:0,pais:0,__v:0});
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 
 /**
  * Create a new Author
